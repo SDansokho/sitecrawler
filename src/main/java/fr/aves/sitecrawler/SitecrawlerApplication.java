0fd49eb39b26d26681040f51f81ce56731c573f8
Samuel Dansokho <samuel.dansokho@carbon-it.com>
2024-04-18 14:17:25 +0200

    Setup app

---
+++ src/main/java/fr/aves/sitecrawler/SitecrawlerApplication.java
@@ -0,0 +1,13 @@
+package fr.aves.sitecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitecrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitecrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/aves/sitecrawler/SitecrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.aves.sitecrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitecrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
